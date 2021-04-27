1734f4f29317e8c17b11cc083b9d7b81bff15f26
naishac <79550754+naishac@users.noreply.github.com>
2021-04-27 16:23:35 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,25 +17,26 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 750);
+	createCanvas(800, 400);
 
 	// fairyVoice.play();
 
-	fairy = createSprite(130, 520);
+	fairy = createSprite(130, 350);
 	fairy.addAnimation("fairyflying",fairyImg);  
-	fairy.scale =0.25;
+	fairy.scale =0.1;
 
 	star = createSprite(650,30);
 	star.addImage(starImg);
 	star.scale = 0.2;
-	star.x = starBody.position.x;
-	star.y = starBody.position.y;
+
 
 	engine = Engine.create();
 	world = engine.world;
 
 	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, starBody);
+
+
 	
 	Engine.run(engine);
 
@@ -44,22 +45,27 @@ function setup() {
 
 function draw() {
   background(bgImg);
+  
+  if(keyDown(LEFT_ARROW)){
+	fairy.velocityX = -2;
+	fairy.velocityY = 0;
+  }
 
+  if(keyDown(RIGHT_ARROW)){
+	fairy.velocityX = 2;
+	fairy.velocityY = 0;
+}
+
+	star.x = starBody.position.x;
+	star.y = starBody.position.y;
 
   drawSprites();
 
 }
 
 function keyPressed(){
-
-	if(keyDown(LEFT_ARROW)){
-		fairy.velocityX = -2;
-		fairy.velocityY = 0;
-	  }
-
-	  if(keyDown(RIGHT_ARROW)){
-		sophia.velocityX = 2;
-		sophia.velocityY = 0;
+	if(keyCode===DOWN_ARROW){
+		Matter.Body.setStatic(starBody,false);
 	}
+}
 
-	
